1af36c99f834b61ef2a02a07514adfe75becb730
Giacomo Calì <caligiacomo0@gmail.com>
2025-06-24 14:58:07 +0200

    deltaTime applicato alla rotazione verticale della camera

--- Assets/Scripts/CameraVerticalLook.cs
+++ Assets/Scripts/CameraVerticalLook.cs
@@ -4,20 +4,18 @@ public class CameraVerticalLook : MonoBehaviour
 {
     public float verticalSens;
     public bool invertVertical;
-    
+
     float mouseY;
-
-
 
     void CameraVerticalRotation()
     {
         if (!invertVertical)
         {
-            mouseY -= Input.GetAxisRaw("Mouse Y") * verticalSens;
+            mouseY -= Input.GetAxisRaw("Mouse Y") * verticalSens * Time.deltaTime;
         }
         else
         {
-            mouseY -= -Input.GetAxisRaw("Mouse Y") * verticalSens;
+            mouseY -= -Input.GetAxisRaw("Mouse Y") * verticalSens * Time.deltaTime;
         }
 
         mouseY = Mathf.Clamp(mouseY, -70, 70);
