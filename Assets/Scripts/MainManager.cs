e5c3b0ad42a3236ac6e00d2b7c807270dde6e4d7
Giacomo Calì <caligiacomo0@gmail.com>
2025-07-04 21:05:21 +0200

    Player and touch controls added to main scene

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -5,6 +5,7 @@ public class MainManager : MonoBehaviour
     private void Start()
     {
         Application.targetFrameRate = 60;
+        QualitySettings.vSyncCount = 0;
         Cursor.lockState = CursorLockMode.None;
     }
 }
