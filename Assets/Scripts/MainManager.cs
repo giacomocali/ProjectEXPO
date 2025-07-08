aba4ba7ddde373534184ee83c0ce2dfb10c52a38
Giacomo Calì <caligiacomo0@gmail.com>
2025-07-08 19:05:01 +0200

    ui polish

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -5,7 +5,7 @@ public class MainManager : MonoBehaviour
     private void Start()
     {
         Time.timeScale = 1f;
-        Application.targetFrameRate = 30;
+        Application.targetFrameRate = 60;
         QualitySettings.vSyncCount = 0;
         Cursor.lockState = CursorLockMode.None;
     }
